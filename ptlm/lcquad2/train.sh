800215500331078c8963dcddf31d9ee4d30d2611
Pranav Ajit Nair <50518113+pranavajitnair@users.noreply.github.com>
2022-04-28 16:42:55 +0530

    Update train.sh

--- ptlm/lcquad2/train.sh
+++ ptlm/lcquad2/train.sh
@@ -1,6 +1,11 @@
 # Training T5-base, T5-small and BART-base
 
+echo "Training T5 base on LCQUAD 2.0"
 python3 Train_T5.py --train_file train_new_mix.pickle --model_name t5-base
+
+echo "Training T5 small on LCQUAD 2.0"
 python3 Train_T5.py --train_file train_new_mix.pickle --model_name t5-small
+
+echo "Training BART base on LCQUAD 2.0"
 python3 Train_BART.py --train_file train_new_mix.pickle --model_name facebook/bart-base
 
